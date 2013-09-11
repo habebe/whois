cf2eb28e6b40913adf0a4f4131e5b7eccca7c179
Henock Abebe <henocka@Henock-Abebes-MacBook-Pro.local>
2013-09-11 09:51:21 -0500

    various errors.

--- scripts/simple_run.sh
+++ scripts/simple_run.sh
@@ -1,5 +1,5 @@
 java -jar build/bootstrap.jar -overwrite
-java -jar build/benchmark.jar -verbose 2 -op_file simple.data -tx_size 1000 -tx_type pipeline
+java -jar build/benchmark.jar -verbose 2 -op_file ./dataset/simple.data -tx_size 1000 -tx_type pipeline
 $IG_HOME/bin/igpipelineagent -loggingProperties properties/logging.properties -userTaskDirectory pipeline/ -bootfile ./data/whois.boot -monitor -idletime 500
 
 
